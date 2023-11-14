f9900b96717abbc5a664ac8318d02a1f1023483c
ClydenPacheco <128603080+ClydenPacheco@users.noreply.github.com>
2023-11-15 00:30:35 +0530

    Update and rename AddTwoNumbers.java to 2_AddTwoNumbers.java

--- LeetCodeSolutions/2_AddTwoNumbers.java
+++ LeetCodeSolutions/2_AddTwoNumbers.java
@@ -12,7 +12,12 @@ Output: [0]
 
 Example 3:
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]*/
+Output: [8,9,9,9,0,0,0,1]
+
+Constraints:
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.*/
 
 /**
  * Definition for singly-linked list.
@@ -24,6 +29,7 @@ Output: [8,9,9,9,0,0,0,1]*/
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode l3 = null;
