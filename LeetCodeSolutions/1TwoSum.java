7608d87388a35b8964a9789587680a4da0cb76cd
ClydenPacheco <128603080+ClydenPacheco@users.noreply.github.com>
2023-11-15 00:29:08 +0530

    Update and rename TwoSum.java to 1TwoSum.java

--- LeetCodeSolutions/1TwoSum.java
+++ LeetCodeSolutions/1TwoSum.java
@@ -13,7 +13,13 @@ Output: [1,2]
 
 Example 3:
 Input: nums = [3,3], target = 6
-Output: [0,1]*/
+Output: [0,1]
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.*/
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
