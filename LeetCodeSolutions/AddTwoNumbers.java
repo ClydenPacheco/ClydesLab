61fbbd4d0f460fb1ddc4d0e482a0d43b43c7ebf8
ClydenPacheco <128603080+ClydenPacheco@users.noreply.github.com>
2023-11-15 00:25:36 +0530

    Create AddTwoNumbers.java

---
+++ LeetCodeSolutions/AddTwoNumbers.java
@@ -0,0 +1,57 @@
+/*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself. 
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = null;
+        int carry = 0;
+        int sum,x,y;
+        while(l1 != null || l2 != null || carry != 0){
+            x = (l1!=null)?l1.val:0;
+            y = (l2!=null)?l2.val:0;
+            sum = x+y+carry;
+            carry = sum/10;
+            sum = sum%10;
+            ListNode newNode = new ListNode(sum);
+            newNode.next = null;
+            if(l3 == null){
+                l3 = newNode;
+            }
+            else{
+                ListNode n = l3;
+                while(n.next != null){
+                    n = n.next;
+                }
+                n.next = newNode;
+            }
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        return l3;
+    }
+}
